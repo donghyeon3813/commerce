08303a3390b394e2fe459edd6bcd89ed02317c3a
donghyeon3813 <ehdgusdl5753@gmail.com>
2024-06-29 00:32:27 +0900

    commerce init

---
+++ src/test/java/com/shop/commerce/CommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shop.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
