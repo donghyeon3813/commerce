68ab2fbd4d82fbef9cd172ee4964ef89e4f7ae5e
donghyeon3813 <ehdgusdl5753@gmail.com>
2024-06-30 21:07:34 +0900

    spring security 초기 설정

---
+++ src/main/java/com/shop/commerce/member/Member.java
@@ -0,0 +1,29 @@
+package com.shop.commerce.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "MEMBER")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Member {
+    @Id
+    @Column(name = "MEMBER_UID")
+    private Long memberUid;
+    @Column(name = "ID")
+    private String Id;
+    @Column(name = "password")
+    private String password;
+
+
+}
